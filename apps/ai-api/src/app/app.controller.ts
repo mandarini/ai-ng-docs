44a2417ef2194511b242ea5d91962cb2634b00bf
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-03-13 14:16:46 -0700

    fixed the nest code

--- apps/ai-api/src/app/app.controller.ts
+++ apps/ai-api/src/app/app.controller.ts
@@ -2,16 +2,17 @@
 import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() // No need to specify the path here, globalPrefix will take care of '/api'
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post('/api')
+  @Post() // This now listens at '/api' due to the globalPrefix
   async handleQuery(@Body('query') query: string, @Res() res): Promise<any> {
     try {
       const responseText = await this.appService.processQuery(query);
       res.send({ message: responseText });
     } catch (error) {
+      console.error(error);
       // Handle errors appropriately
       res.status(500).send({ error: 'Internal Server Error' });
     }
